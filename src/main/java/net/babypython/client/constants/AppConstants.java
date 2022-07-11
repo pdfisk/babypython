e58ab0ebb4b09b771bf10285970064b098d88000
peter fisk <peter.fisk@gmail.com>
2022-07-11 13:44:37 -0400

    Add docs reference to welcome page

--- src/main/java/net/babypython/client/constants/AppConstants.java
+++ src/main/java/net/babypython/client/constants/AppConstants.java
@@ -37,7 +37,7 @@ public class AppConstants {
     public static final boolean SHOW_TWITTER = true;
     public static final boolean SHOW_YOUTUBE = true;
     //
-    public static final String TIMESTAMP = "2022-07-11T14:44:38Z";
+    public static final String TIMESTAMP = "2022-07-11T17:22:52Z";
     public static final String VERSION = "1.0.0";
     public static final String BUILD_NUMBER = "12";
 }
--- src/main/java/net/babypython/client/ui/windows/welcome/widgets/baby_python_page/Text.java
+++ src/main/java/net/babypython/client/ui/windows/welcome/widgets/baby_python_page/Text.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2022 Peter Fisk
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,6 +25,7 @@ package net.babypython.client.ui.windows.welcome.widgets.baby_python_page;
 
 import com.google.gwt.dom.client.Style;
 import net.babypython.client.ui.constants.CommonWindowConstants;
+import net.babypython.client.ui.constants.UrlConstants;
 import net.babypython.client.ui.gwt.widgets.GwtTextHtml;
 import net.babypython.client.vm.containers.lists.StringList;
 
@@ -41,6 +42,12 @@ public class Text extends GwtTextHtml {
         stringList.add(WELCOME_2);
         stringList.add(NEWLINE_2);
         stringList.add(CLICK_OPEN_PROJECTS);
+        stringList.add(NEWLINE_2);
+        stringList.add(NEWLINE_2);
+        stringList.add(READ_DOCS_1);
+        stringList.add(READ_DOCS_2);
+        stringList.add(READ_DOCS_3);
+        stringList.add(READ_DOCS_4);
     }
 
     protected void setStyles(Style style) {
@@ -49,8 +56,12 @@ public class Text extends GwtTextHtml {
         style.setPaddingTop(15, Style.Unit.PX);
     }
 
-    static final String CLICK_OPEN_PROJECTS = "Click on the 'Open Projects' button below to get started." ;
+    static final String CLICK_OPEN_PROJECTS = "Click on the 'Open Projects' button below to get started.";
     static final String NEWLINE_2 = "<br><br>";
+    static final String READ_DOCS_1 = "The docs can be found here: ";
+    static final String READ_DOCS_2 = "<a href=\"" + UrlConstants.Docs + "\" target=\"_blank\">";
+    static final String READ_DOCS_3 = "BabyPython Docs";
+    static final String READ_DOCS_4 = "</a>";
     static final String WELCOME_1 = "Welcome to Baby Python - ";
     static final String WELCOME_2 = "the learning environment for beginners of all ages.";
 }
