ca8a8e9933dc017c05d36e539223e24ae9d515af
peter fisk <peter.fisk@gmail.com>
2022-07-25 14:42:42 -0400

    Updated version

--- src/main/java/net/babypython/client/constants/AppConstants.java
+++ src/main/java/net/babypython/client/constants/AppConstants.java
@@ -25,15 +25,15 @@ package net.babypython.client.constants;
  */
 
 public class AppConstants {
-    public static final boolean IS_DEBUG = true;
+    public static final boolean IS_DEBUG = false;
     //
     public static final boolean SHOW_DEBUG = false;
     public static final boolean SHOW_MERCHANDISE = true;
     public static final boolean SHOW_PATREON = true;
     //
-    public static final String TIMESTAMP = "2022-07-25T15:53:03Z";
+    public static final String TIMESTAMP = "2022-07-25T18:39:36Z";
     public static final String VERSION = "1.0.0";
-    public static final String BUILD_NUMBER = "43";
+    public static final String BUILD_NUMBER = "44";
 }
 
 // https://www.utctime.net/gmt-time-now
